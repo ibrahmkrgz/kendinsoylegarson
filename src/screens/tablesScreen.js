fed9f007ce38842c8995b0f933830b876d033ecc
ibrahim karagoz <ibrahm.krgz@gmail.com>
2021-03-10 21:54:05 +0300

    added new page

--- src/screens/tablesScreen.js
+++ src/screens/tablesScreen.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, TouchableWithoutFeedback, BackHandler, Text, Image, ScrollView, Modal } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, BackHandler, Text, Image, ScrollView, Modal } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 import axios from 'axios';
+import { Title } from 'native-base';
 import { connect } from 'react-redux';
 import { navigate } from '../services/navigationService';
 import { withNavigation } from 'react-navigation';
@@ -49,13 +50,85 @@ const TableScreen = ({ errorText, ...props }) => {
     return (
         <View>
             {/* {isLoading && <SkeletonDetail />} */}
-            <ScrollView style={styles.scrollView}>
+            <View style={{ backgroundColor: '#DE3C4B', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', paddingVertical: 15, paddingHorizontal: 10 }}>
+
                 <View>
-                    <Text>
-                        Table page
-                  </Text>
+                    <Title style={{ color: '#f9f7f7' }}>Masalar</Title>
                 </View>
-            </ScrollView>
+            </View>
+            <View style={{ flexDirection: 'row', flexWrap: 'wrap', alignItems: 'flex-start', justifyContent: 'center' }}>
+                <TouchableOpacity style={{
+                    margin: 10, borderRadius: 10, background: '#fff',
+                    borderColor: '#f9f7f7',
+                    borderWidth: 1,
+                    overflow: 'hidden',
+                    backgroundColor: "#0000",
+                    shadowColor: '#000',
+                    shadowOffset: { width: 0, height: 1 },
+                    shadowOpacity: 0.6,
+                    shadowRadius: 2,
+                    elevation: 3
+                }}  >
+
+                    <View style={{
+                        alignItems: 'center', flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 10, backgroundColor: "#fcfcfc", height: 150,
+
+
+                    }}>
+                        <View style={{ paddingVertical: 10 }}>
+                            <View style={{ paddingVertical: 5 }}>
+                                <Text style={{ fontSize: 20, fontFamily: 'Lobster-Regular' }}>
+                                    bildirim
+                        </Text>
+                            </View>
+                            <View>
+                                <Text style={{ fontSize: 13, color: '#ccc5c5', fontFamily: 'Lobster-Regular' }}>
+                                    detay
+                        </Text>
+                            </View>
+                        </View>
+                        <View style={{ backgroundColor: "#fcfcfc" }}>
+
+                            <View style={{ paddingVertical: 0, fontFamily: 'Lobster-Regular' }}><Text style={{ fontSize: 13, color: '#ccc5c5' }}>₺ iki kişi için</Text></View>
+                        </View>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity style={{
+                    margin: 10, borderRadius: 10, background: '#fff',
+                    borderColor: '#f9f7f7',
+                    borderWidth: 1,
+                    overflow: 'hidden',
+                    backgroundColor: "#0000",
+                    shadowColor: '#000',
+                    shadowOffset: { width: 0, height: 1 },
+                    shadowOpacity: 0.6,
+                    shadowRadius: 2,
+                    elevation: 3
+                }}  >
+
+                    <View style={{
+                        alignItems: 'center', flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 10, backgroundColor: "#fcfcfc", height: 150,
+
+                    }}>
+                        <View style={{ paddingVertical: 10 }}>
+                            <View style={{ paddingVertical: 5 }}>
+                                <Text style={{ fontSize: 20, fontFamily: 'Lobster-Regular' }}>
+                                    bildirim
+                        </Text>
+                            </View>
+                            <View>
+                                <Text style={{ fontSize: 13, color: '#ccc5c5', fontFamily: 'Lobster-Regular' }}>
+                                    detay
+                        </Text>
+                            </View>
+                        </View>
+                        <View style={{ backgroundColor: "#fcfcfc" }}>
+
+                            <View style={{ paddingVertical: 0, fontFamily: 'Lobster-Regular' }}><Text style={{ fontSize: 13, color: '#ccc5c5' }}>₺ iki kişi için</Text></View>
+                        </View>
+                    </View>
+                </TouchableOpacity>
+            </View>
         </View >
     )
 }
