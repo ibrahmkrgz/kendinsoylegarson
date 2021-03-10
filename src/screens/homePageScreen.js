f5b758364b9d22b75804811e66c12cbd9dcfeb09
ibrahim karagoz <ibrahm.krgz@gmail.com>
2021-03-10 18:51:06 +0300

    added swipable page

--- src/screens/homePageScreen.js
+++ src/screens/homePageScreen.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, TouchableWithoutFeedback, BackHandler, Text, Image, ScrollView, Modal } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, BackHandler, Text, Image, ScrollView, Animated } from 'react-native';
+import Swipeable from 'react-native-gesture-handler/Swipeable'
+import { Title } from 'native-base';
 import AsyncStorage from '@react-native-community/async-storage';
 import axios from 'axios';
 import { connect } from 'react-redux';
@@ -45,17 +47,75 @@ const HomeScreen = ({ errorText, ...props }) => {
 
     // }, []);
 
+    const RightAction = (progress, dragX) => {
+
+        return (
+            <View
+                style={{
+                    backgroundColor: '#DE3C4B',
+                    justifyContent: 'center',
+                    marginTop: 5,
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                    flexDirection: 'row',
+                    paddingVertical: 20,
+                    paddingHorizontal: 15,
+                    shadowColor: '#ede6e6',
+                    shadowOpacity: 0.1,
+                    flex: 1
+                }}>
+                <Animated.Text
+                    style={{
+                        color: 'white',
+                        paddingHorizontal: 10,
+                        fontWeight: '600',
+                        // transform: [{ scale }]
+                    }}>
+                    Tamamlandı
+            </Animated.Text>
+            </View>
+        )
+    }
+
     console.log(responseData, 'zeynom menuu')
     return (
         <View>
             {/* {isLoading && <SkeletonDetail />} */}
-            <ScrollView style={styles.scrollView}>
+            <View style={{ backgroundColor: '#DE3C4B', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', paddingVertical: 15, paddingHorizontal: 10 }}>
+
                 <View>
-                    <Text>
-                        Home page
-                  </Text>
+                    <Title style={{ color: '#f9f7f7' }}>Bildirimler</Title>
                 </View>
-            </ScrollView>
+            </View>
+            <View style={{ backgroundColor: '#ede8e8' }}>
+                <Swipeable key="1" renderRightActions={() => RightAction()}>
+                    <View style={{
+                        width: '100%',
+                        marginTop: 5,
+                        alignItems: 'center',
+                        justifyContent: 'space-between',
+                        flexDirection: 'row',
+                        backgroundColor: '#f9f7f7',
+                        paddingVertical: 20,
+                        paddingHorizontal: 15,
+                        borderRadius: 7,
+                        shadowColor: '#ede6e6',
+                        shadowOpacity: 0.1,
+                        elevation: 2,
+                        shadowOffset: {
+                            width: 0,
+                            height: 3,
+                        },
+                    }}>
+                        <View style={{ width: '60%' }}>
+                            <Text style={{ fontSize: 15, fontWeight: '600' }}>
+                                Bildirim
+                                </Text>
+                        </View>
+
+                    </View>
+                </Swipeable>
+            </View>
         </View >
     )
 }
